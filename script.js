3b207eadd21e64df19dc6b0ee1c2aa8736063723
SKEIILATT <javimacias619@gmail.com>
2025-04-08 21:12:40 -0500

    Official Launch

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 /// CONSUMIR EL API
-const apiKey1 = '';
+const apiKey1 = '9ab8d09adbec4f7f8ff0e8a2ca5010ee';
 const contenedor_noticias = document.getElementById("contenido-noticias");
 const input = document.getElementById("buscar-noticias");
+let filtroActual = "publishedAt";
 
 document.querySelectorAll(".boton-filtro-noticias").forEach(boton =>{
     boton.addEventListener("click",cambiarColor)
 })
+
 
 
 //Función para cambiar el color de la barra de destacados, recientes, populares
@@ -16,14 +18,18 @@ function cambiarColor(){
     })
     this.classList.remove("botones-filtros-noActivo");
     this.classList.add("botones-filtros-activo");
+    filtroActual = this.textContent.toLowerCase()
+    const textoBusqueda = input.value.trim();
+    if(textoBusqueda!==""){
+        getNewsbySearch(textoBusqueda, filtroActual);
+    }
 }
 
 
 
-
-//Función para obtener las noticias mediante la barra de busqueda
-async function getNewsbySearch(query){
-    let url = `https://newsapi.org/v2/everything?q=${query}&apiKey=${apiKey1}`
+//Función para obtener las noticias mediante la barra de busqueda 
+async function getNewsbySearch(query, state){
+    let url = `https://newsapi.org/v2/everything?q=${query}&sortBy=${state}&apiKey=${apiKey1}`
     try{
         const response = await fetch(url)
         const data = await response.json()
@@ -41,7 +47,7 @@ input.addEventListener("keydown", function(event){
         if(texto==""){
             alert("Ingrese su busqueda ")
         }
-        getNewsbySearch(texto);
+        getNewsbySearch(texto, filtroActual);
     }
 });
 
@@ -57,13 +63,26 @@ async function getNewsbyCategory(category="health"){
         console.log("Error Obtenido en las noticias", error)
     }
 }
+//Función para obtener las noticias desde la API con una fuente dada
+async function getNewsbySource(fuente){
+    let url = `https://newsapi.org/v2/everything?domains=${fuente}&apiKey=${apiKey1}`;
+    try{
+        const response = await fetch(url);
+        const dato = await response.json();
+        displayNews(dato.articles)
+    }
+    catch(error){
+        console.log("Error Obtenido en las noticias", error)
+    }
+}
+
 function displayNews(articles){
     contenedor_noticias.innerHTML = "";
     articles.forEach(article =>{
         const newCard = document.createElement("div")
         newCard.classList.add("news-card");
         newCard.innerHTML=`
-            <img class="img-api" src="${article.urlToImage || images/imgnot.jpg}" alt="news-Image">
+            <img class="img-api" src="${article.urlToImage || "images/imgnot.jpg"}" alt="news-Image">
             <h3>${article.title}</h3>
             <p>${article.description || "No hay descripción disponible"}</p>
             <a href="${article.url} target="_blank">Leer Mas</a>
@@ -79,4 +98,13 @@ document.querySelectorAll(".boton-filtro").forEach(boton=>{
         getNewsbyCategory(categoria)
     })
 })
-getNewsbyCategory();    
+getNewsbyCategory();   
+
+
+document.querySelectorAll(".boton-filtro-fuente").forEach(boton=>{
+    boton.addEventListener("click", function(){
+        const fuente = this.id.replace("filtro-","")
+        console.log(fuente)
+        getNewsbySource(fuente)
+    })
+})
